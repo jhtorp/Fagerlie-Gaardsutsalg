4362995df71f5d7ed968ee1b24c0a06df30977d2
jhtorp <jhtorp@hotmail.com>
2022-11-19 22:28:44 +0100

    Fiksa så burger menyen forsvinner på link klikk

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ document.addEventListener('DOMContentLoaded', nav)
 function nav() {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.topnav');
+    const navLink = document.querySelector('.nav-link');
     burger.addEventListener('click', () => {
         nav.classList.toggle('show')
     })
+
+    navLink.addEventListener('click', () => {
+        nav.classList.toggle('show')
+    })
+
+
 }
+
 
 //Preload fix
 
