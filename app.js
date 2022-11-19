4b3e60215377b9b2f274d3cc8b069ca4e16d56f5
jhtorp <jhtorp@hotmail.com>
2022-11-19 21:59:35 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Burger on Click
+
+document.addEventListener('DOMContentLoaded', nav)
+function nav() {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.topnav');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('show')
+    })
+}
+
+//Preload fix
+
+setTimeout(function () {
+    document.body.className = "";
+}, 500);
