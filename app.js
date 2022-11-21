106973e647448ca5a9ccd71033517a206b6169a6
jhtorp <jhtorp@hotmail.com>
2022-11-21 11:01:45 +0100

    Lagt til produkter side + andre småendringer

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ document.addEventListener('DOMContentLoaded', nav)
 function nav() {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.topnav');
-    const navLink = document.querySelector('.nav-link');
     burger.addEventListener('click', () => {
         nav.classList.toggle('show')
     })
+    const navLink = document.querySelectorAll('.nav-link').forEach(navLink => {
+        navLink.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
 
-    navLink.addEventListener('click', () => {
-        nav.classList.toggle('show')
     })
 
 
+
 }
+
+
 
 
 //Preload fix
